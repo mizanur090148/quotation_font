e8f6f79c16a5e2962b23275542e836eaae543f4a
Mizanur Rahman <mizan@skylarksoft.com>
2020-06-29 01:19:05 +0600

    quotation service list edit

--- src/components/QuotationList.vue
+++ src/components/QuotationList.vue
@@ -31,9 +31,11 @@
                                 <th scope="col">QID</th>
                                 <th scope="col">To</th>
                                 <th width="33%" scope="col">Subject</th>
-                                <th scope="col">Quotation Date</th>
-                                <th scope="col">Total Discount</th>
-                                <th scope="col">Total Amount</th>
+                                <th scope="col">Q. Date</th>
+                                <th scope="col">Discount</th>
+                                <th scope="col">Amount</th>
+                                <th scope="col">Services</th>
+                                <th title="Terms & Conditions" scope="col">T & C</th>
                                 <th scope="col">Actions</th>
                             </tr>
                         </thead>
@@ -46,6 +48,16 @@
                                 <td>{{ quotation.quotation_date }}</td>                               
                                 <td>{{ quotation.total_discount }}</td>
                                 <td>{{ quotation.total_without_discount }}</td>
+                                <td>
+                                    <button type="button" class="btn btn-success btn-xs" @click="addEditDeleteServices(quotation.id)" title="Add/Edit/Delete">
+                                        <i class="fas fa fa-edit"></i>
+                                    </button>
+                                </td>
+                                <td>
+                                    <button type="button" class="btn btn-success btn-xs" @click="edit(quotation)" title="Add/Edit/Delete">
+                                        <i class="fas fa fa-edit"></i>
+                                    </button>
+                                </td>
                                 <td>
                                     <!-- <router-link
                                        :to="'/view-quotation?qid=' + quotation.id"
@@ -72,6 +84,46 @@
                 </div>
             </div>
         </div>
+
+        <!-- Add, Edit or Delete Services Modal  -->
+        <modal name="serviceModal" :width="650" :height="420">
+            <div class="col-xs-12 col-sm-12">
+                <div class="card">
+                    <div class="card-header font-weight-bold">Add, Edit or Delete services</div>
+                    <div class="card-body card-block" ref="quotationContainer">
+                        <form @submit.prevent="">
+                            <div class="row">
+                                <div class="col-4">
+                                    <div class="form-group">
+                                        <label for="vendor_name" class="form-control-label font-weight-bold">Vendor Name</label>
+                                        <!-- <input type="text" v-model="vendor_form.vendor_name" class="form-control" placeholder="Enter vendor name" :class="{ 'is-invalid': vendor_errors.vendor_name }"><div class="v-error" v-if="vendor_errors.vendor_name">{{ vendor_errors.vendor_name[0] }}</div> -->
+                                    </div>
+                                </div>
+                                <div class="col-4">
+                                    <div class="form-group">
+                                        <label for="vendor_no" class="form-control-label font-weight-bold">Vendor No.</label>
+                                       <!--  <input type="text" v-model="vendor_form.vendor_no" class="form-control" placeholder="Enter vendor no" :class="{ 'is-invalid': vendor_errors.vendor_no }"><div class="v-error" v-if="vendor_errors.vendor_no">{{ vendor_errors.vendor_no[0] }}</div>            -->                            
+                                    </div>
+                                </div>
+                                <div class="col-4">
+                                    <div class="form-group">
+                                        <label for="trn_no" class="form-control-label font-weight-bold">Trn No.</label>                                      
+                                        <!-- <input type="text" v-model="vendor_form.trn_no" class="form-control" placeholder="Enter trn no" :class="{ 'is-invalid': vendor_errors.trn_no }"><div class="v-error" v-if="vendor_errors.trn_no">{{ vendor_errors.trn_no[0] }}</div>   -->
+                                    </div>
+                                </div>
+                            </div>                           
+                            <div class="form-group row m-t-md">
+                                <div class="col-sm-offset-4 col-sm-12 text-center">
+                                    <button type="submit" class="btn btn-info btn-rounded m-b-10 m-l-5">Submit</button>
+                                    <button @click="reset()" type="button" class="btn btn-danger btn-rounded m-b-10 m-l-5">Reset</button>
+                                </div>
+                            </div> 
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </modal>
+        <!-- End Add, Edit or Delete services Modal  -->
         <vue-snotify></vue-snotify>
     </div>
 </template>
@@ -140,7 +192,13 @@ export default {
         return string.substring(0, 45) + '...';
       }  
     },
-    methods: {
+    methods: { 
+        addEditDeleteServices() {
+            // this.editMode = false   
+            /*this.form.reset();*/
+            this.$modal.show('serviceModal')
+        },
+
         getQuotations() {
             const loader = this.$loading.show({
                 container: this.$refs.attendanceTable,
